2881aa529c1b51c9cf91ac23b4bab7d528aaf995
jamtechdev <jamtechdev@gmail.com>
2025-02-04 16:32:15 +0530

    changig xox

--- controller/profile/profile.controller.js
+++ controller/profile/profile.controller.js
@@ -7,7 +7,7 @@ const {
 const User = db.users;
 const Region = db.Regions;
 const Profile = db.model_profile;
-
+const Content = db.Content;
 const Category = db.ContentCategory;
 
 // create profile api
@@ -275,6 +275,296 @@ exports.updateModelProfile = async (req, res) => {
       .json({ code: 500, success: false, error: "Internal server error" });
   }
 };
+// upload Aavart and Update
+exports.uploadAvatar = async (req, res) => {
+  try {
+    const user = req?.user;
+    const avatar = req?.file;
+    if (!avatar) {
+      return res.status(400).json({
+        code: 400,
+        success: false,
+        message: "No file uploaded",
+      });
+    }
+    const existing_user = await User.findOne({ where: { id: user?.userId } });
+    if (!existing_user) {
+      return res.status(404).json({
+        code: 404,
+        success: false,
+        message: "User not found",
+      });
+    }
+    const uploadedAvatar = await cloudinaryImageUpload(avatar?.path, "image");
+    if (uploadedAvatar?.error) {
+      return res.status(400).json({
+        code: 400,
+        success: false,
+        message: uploadedAvatar?.error,
+      });
+    }
+    // Update user's avatar
+    await User.update(
+      { avatar: uploadedAvatar },
+      { where: { id: user?.userId } }
+    );
+    // Fetch the updated user data
+    const updatedUser = await User.findOne({
+      where: { id: user?.userId },
+      attributes: [
+        "id",
+        "name",
+        "email",
+        "avatar",
+        "address",
+        "phone_number",
+        "birthdate",
+        "social_links",
+        "bio",
+      ],
+    });
+    return res.status(200).json({
+      code: 200,
+      success: true,
+      message: "Avatar uploaded successfully",
+      data: updatedUser,
+    });
+  } catch (error) {
+    console.error("Error uploading avatar:", error);
+    return res.status(500).json({
+      code: 500,
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
 
+exports.uploadImage = async (req, res) => {
+  try {
+    const user = req?.user;
+    const image = req.file;
+    if (!image) {
+      return res
+        .status(400)
+        .json({ code: 400, success: false, message: "No image uploaded" });
+    }
+    const model_exists = await Profile.findOne({
+      where: { user_id: user.userId },
+    });
+    if (!model_exists) {
+      return res.status(404).json({
+        code: 404,
+        success: false,
+        message: "No such model profile exists",
+      });
+    }
 
+    const imageUri = await cloudinaryImageUpload(image.path, "image");
 
+    const response = req.body.profile_picture
+      ? await Profile.update(
+          { profile_picture: imageUri },
+          { where: { id: model_exists.id } }
+        )
+      : await Profile.update(
+          { cover_photo: imageUri },
+          { where: { id: model_exists.id } }
+        );
+
+    const modelProfile = await Profile.findOne({
+      where: { id: model_exists.id },
+    });
+    return res.status(200).json({
+      code: 200,
+      status: true,
+      message: "Image uploaded successfully",
+      data: modelProfile,
+    });
+  } catch (error) {
+    console.error("Error uploading avatar:", error);
+    res
+      .status(500)
+      .json({ code: 500, success: false, error: "Internal server error" });
+  }
+};
+
+exports.createContent = async (req, res) => {
+  const { userId } = req?.user;
+  const { title, description, content_type, category_id } = req.body;
+  const user = await User.findOne({ where: { id: userId } });
+
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).json({
+      code: 400,
+      success: false,
+      message: "Data is required to create content",
+    });
+  }
+  try {
+    const data = {
+      title,
+      description,
+      content_type,
+      category_id,
+      user_id: userId,
+      region_id: user.region_id,
+    };
+    const content = await Content.create(data);
+    return res.status(201).json({
+      code: 201,
+      success: true,
+      message: "Content created successfully",
+      data: content,
+    });
+  } catch (error) {
+    console.error("Error creating content", error);
+    res
+      .status(500)
+      .json({ code: 500, success: false, error: "Internal server error" });
+  }
+};
+
+exports.uploadContent = async (req, res) => {
+  try {
+    const user = req?.user;
+    const { contentId } = req.params;
+    const mediaFile = req?.file;
+    const content_exists = await Content.findOne({
+      where: { id: contentId, user_id: user.userId },
+    });
+    if (!content_exists) {
+      return res
+        .status(404)
+        .json({ code: 404, success: false, message: "Content not found" });
+    }
+    if (!mediaFile) {
+      return res
+        .status(400)
+        .json({ code: 400, success: false, message: "No media file attached" });
+    }
+    const mediaFileUrl = await cloudinaryImageUpload(
+      mediaFile.path,
+      content_exists.content_type
+    );
+    await Content.update(
+      { media_url: mediaFileUrl },
+      { where: { user_id: user.userId, id: contentId } }
+    );
+
+    const content = await Content.findOne({
+      where: { user_id: user.userId, id: contentId },
+    });
+    return res.status(200).json({
+      code: 200,
+      success: true,
+      message: "Content uploaded successfully",
+      data: content,
+    });
+  } catch (error) {
+    console.error("Error uploading content", error);
+    res
+      .status(500)
+      .json({ code: 500, success: false, error: "Internal server error" });
+  }
+};
+
+exports.getContent = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const { userId } = req?.user;
+    const user = await User.findOne({ where: { id: userId } });
+
+    const offset = (page - 1) * limit;
+    const { count, rows } = await Content.findAndCountAll({
+      where: { region_id: user.region_id },
+      limit,
+      offset,
+      //   include: [{
+      //   model: Category,
+      //   as: "category"
+      // }]
+    });
+    if (rows.length === 0) {
+      return res
+        .status(404)
+        .json({ code: 404, success: false, message: "Content not found" });
+    }
+    const pagination = {
+      totalPages: Math.ceil(count / limit),
+      totalDocs: count,
+      currentPage: page,
+    };
+    return res.status(200).json({
+      code: 200,
+      success: true,
+      total: count,
+      region: user.region_id,
+      message: "Content fetched successfully",
+      pagination: pagination,
+      data: rows,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ code: 500, success: false, error: "Internal server error" });
+  }
+};
+
+exports.updateContent = async (req, res) => {
+  try {
+    const user = req?.user;
+    const { contentId } = req.params;
+    const { status, title, description, content_type, category_id } = req?.body;
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        code: 400,
+        success: false,
+        message: "Data fields required to update content",
+      });
+    }
+    const content = await Content.findOne({
+      where: { id: contentId, user_id: user.userId },
+    });
+    if (!content) {
+      return res
+        .status(404)
+        .json({ code: 404, success: false, message: "Content not found" });
+    }
+    const updateContent = await Content.update(
+      {
+        status,
+        title,
+        description,
+        content_type,
+        category_id,
+      },
+      {
+        where: { id: contentId, user_id: user.userId },
+      }
+    );
+    if (updateContent[0] === 0) {
+      return res.status(404).json({
+        code: 400,
+        error: true,
+        message: "Unable to update content",
+      });
+    }
+    const response = await Content.findOne({
+      where: { id: contentId, user_id: user.userId },
+    });
+
+    return res.status(200).json({
+      code: 200,
+      success: true,
+      message: "Content data updated successfully",
+      data: response,
+    });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ code: 500, success: false, message: "Internal server error" });
+  }
+};
--- routes/Api/users/usersProfile.route.js
+++ routes/Api/users/usersProfile.route.js
@@ -3,13 +3,20 @@ const {
   getModalProfileById,
   createModalProfile,
   updateUserById,
-  // getMyProfile,
+  getMyProfile,
   updateModelProfile,
-  // uploadAvatar,
+  uploadImage,
+  uploadAvatar,
+  createContent,
+  uploadContent,
+  getContent,
+  updateContent,
 } = require("../../../controller/profile/profile.controller");
 const { authenticateToken, userProtect } = require("../../../middleware/middleware");
 const { upload } = require("../../../middleware/multerConfig");
-
+const {
+  cloudinaryImageUpload,
+} = require("../../../services/cloudinaryService");
 const router = express.Router();
 
 // create modal Profile api
@@ -21,19 +28,23 @@ router.get(
   getModalProfileById
 );
 // get my profile
-// router.get("/get-my-profile", authenticateToken, getMyProfile);
+router.get("/get-my-profile", authenticateToken, getMyProfile);
 // update my profile
 router.put("/my-profile-update", authenticateToken, updateUserById);
 router.put('/update-model-profile', authenticateToken, updateModelProfile)
-
+router.put('/upload-image', authenticateToken, upload.single('image'), uploadImage)
+router.post('/create-content', authenticateToken, upload.single('content'), createContent)
+router.put('/upload-content/:contentId', authenticateToken, upload.single('mediaFile'), uploadContent)
+router.get('/get-content/:contentId', authenticateToken, getContent)
+router.put('/update-content/:contentId', authenticateToken, updateContent)
 
 // upload avatar
-// router.post(
-//   "/upload-avatar",
-//   authenticateToken,
-//   userProtect,
-//   upload.single("avatar"),
-//   uploadAvatar
-// );
+router.post(
+  "/upload-avatar",
+  authenticateToken,
+  userProtect,
+  upload.single("avatar"),
+  uploadAvatar
+);
 
 module.exports = router;
